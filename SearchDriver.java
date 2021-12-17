eeb845f695bdd5a1cb9f782104c29b1d681e8554
bli31 <bli31@stuy.edu>
2021-12-17 12:34:30 -0500

    new

--- SearchDriver.java
+++ SearchDriver.java
@@ -1,15 +1,29 @@
 public class SearchDriver{
-    public static void main(String[] args){
-      Comparable[] iArr = new Integer[10000];
+  static Comparable[] iArr = new Integer[10000];
+
+    public static long recordTime_B(){
+      long startTime = System.currentTimeMillis();
       for (int i = 0; i < iArr.length; i++){
-        iArr[i] = i * 2;
+        BinSearch.binSearch(iArr, iArr[i]); // runs binary search on each element of iArr
+      }
+      long elapsedTime = System.currentTimeMillis() - startTime;
+      return elapsedTime / iArr.length;
+    }
+
+    public static long recordTime_I(){
+      long startTime = System.currentTimeMillis();
+      for (int i = 0; i < iArr.length; i++){
+        LinSearch.linSearch(iArr, iArr[i]); // runs linear search on each element of iArr
+      }
+      long elapsedTime = System.currentTimeMillis() - startTime;
+      return elapsedTime / iArr.length;
+    }
+
+    public static void main(String[] args){
+      for (int i = 0; i < iArr.length; i++){
+        iArr[i] = i*2;
       }
 
-      long init = System.nanoTime();
-
-      LinSearch.linSearch(iArr, 2834);
-
-      System.out.println("elapsed time: " + (System.nanoTime() - init));
     }
 
 }
