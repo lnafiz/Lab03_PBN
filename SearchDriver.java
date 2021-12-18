5a1296817bc72f6b6308cddde66cfc544427047a
Brian Li <bli31@stuy.edu>
2021-12-18 15:09:44 -0500

    first try

--- SearchDriver.java
+++ SearchDriver.java
@@ -1,29 +1,32 @@
 public class SearchDriver{
-  static Comparable[] iArr = new Integer[10000];
-
-    public static long recordTime_B(){
+    public static long recordTime_B(Comparable[] iArr){
       long startTime = System.currentTimeMillis();
       for (int i = 0; i < iArr.length; i++){
         BinSearch.binSearch(iArr, iArr[i]); // runs binary search on each element of iArr
       }
       long elapsedTime = System.currentTimeMillis() - startTime;
-      return elapsedTime / iArr.length;
+      // return elapsedTime / iArr.length; // returns avg time for each search
+      return elapsedTime; // returns total time for search of all items
     }
 
-    public static long recordTime_I(){
+    public static long recordTime_I(Comparable[] iArr){
       long startTime = System.currentTimeMillis();
       for (int i = 0; i < iArr.length; i++){
         LinSearch.linSearch(iArr, iArr[i]); // runs linear search on each element of iArr
       }
       long elapsedTime = System.currentTimeMillis() - startTime;
-      return elapsedTime / iArr.length;
+      // return elapsedTime / iArr.length; // returns avg time for each search
+      return elapsedTime; // returns total time for search of all items
     }
 
     public static void main(String[] args){
-      for (int i = 0; i < iArr.length; i++){
-        iArr[i] = i*2;
+      Comparable[] test = new Integer[1000000];
+      for (int i = 0; i < test.length; i++){ // populates test
+        test[i] = i*2;
       }
 
+      System.out.println("total time for linear search within test (ms): \t" + recordTime_I(test));
+      System.out.println("total time for binary search within test (ms): \t" + recordTime_B(test));
     }
 
 }
