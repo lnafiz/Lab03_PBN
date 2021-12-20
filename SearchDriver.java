35bc731e59854af63027a0322f9b4fe91941ace6
Brian Li <61071032+bli31@users.noreply.github.com>
2021-12-20 12:26:26 -0500

    updated

--- SearchDriver.java
+++ SearchDriver.java
@@ -1,3 +1,8 @@
+// Peanut Butter and Nutella - Prattay Dey + Winnie, Brian Li + Robert, Nafiz Labib + Martha
+// APCS pd6
+// Lab03
+// 2021-12-21
+
 public class SearchDriver{
     public static long recordTime_B(Comparable[] iArr){
       long startTime = System.currentTimeMillis();
@@ -20,13 +25,48 @@ public class SearchDriver{
     }
 
     public static void main(String[] args){
-      Comparable[] test = new Integer[1000000];
-      for (int i = 0; i < test.length; i++){ // populates test
-        test[i] = i*2;
+      long[] binTimes = new long[3];
+      long[] linTimes = new long[3];
+
+      Comparable[] test0 = new Integer[200000];
+      for (int i = 0; i < test0.length; i++){ // populates test
+        test0[i] = i*2;
       }
 
-      System.out.println("total time for linear search within test (ms): \t" + recordTime_I(test));
-      System.out.println("total time for binary search within test (ms): \t" + recordTime_B(test));
+      Comparable[] test1 = new Integer[400000];
+      for (int i = 0; i < test1.length; i++){ // populates test
+        test1[i] = i*2;
+      }
+
+      Comparable[] test2 = new Integer[600000];
+      for (int i = 0; i < test2.length; i++){ // populates test
+        test2[i] = i*2;
+      }
+
+      // populates time arrays
+      binTimes[0] = recordTime_B(test0);
+      binTimes[1] = recordTime_B(test1);
+      binTimes[2] = recordTime_B(test2);
+      linTimes[0] = recordTime_I(test0);
+      linTimes[1] = recordTime_I(test1);
+      linTimes[2] = recordTime_I(test2);
+
+
+      System.out.println("total time for linear search within test0 (ms): " + linTimes[0]);
+      System.out.println("total time for binary search within test0 (ms): " + binTimes[0]);
+      System.out.println("total time for linear search within test1 (ms): " + linTimes[1]);
+      System.out.println("total time for binary search within test1 (ms): " + binTimes[1]);
+      System.out.println("total time for linear search within test2 (ms): " + linTimes[2]);
+      System.out.println("total time for binary search within test2 (ms): " + binTimes[2]);
+
+
+      System.out.println("avg time for linear search within test0 (ms): \t\t" + (linTimes[0] / (double)test0.length));
+      System.out.println("avg time for binary search within test0 (ms): \t\t" + (binTimes[0] / (double)test0.length));
+      System.out.println("avg time for linear search within test1 (ms): \t\t" + (linTimes[1] / (double)test1.length));
+      System.out.println("avg time for binary search within test1 (ms): \t\t" + (binTimes[1] / (double)test1.length));
+      System.out.println("avg time for linear search within test2 (ms): \t\t" + (linTimes[2] / (double)test2.length));
+      System.out.println("avg time for binary search within test2 (ms): \t\t" + (binTimes[2] / (double)test2.length));
+
     }
 
 }
