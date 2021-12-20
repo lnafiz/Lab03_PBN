67e418f2e1bf5788dd127870d9ee25bc51f945b8
Prattay Dey <73182203+prattaydey@users.noreply.github.com>
2021-12-20 12:00:05 -0500

    Update BinSearch.java

--- BinSearch.java
+++ BinSearch.java
@@ -1,7 +1,6 @@
-// Clyde "Thluffy" Sinclair
-// APCS1 pd0
-// HW48 -- implementing linear and binary search on an ArrayList of Comparables
-// 2021-12-16r
+// Peanut Butter and Nutella - Prattay Dey + Winnie, Brian Li + Robert, Nafiz Labib + Martha
+// APCS pd6
+// Lab03
 
 /**
    class BinSearch
